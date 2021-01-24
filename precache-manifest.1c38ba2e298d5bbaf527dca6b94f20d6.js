4ed5186d399036b882ee7b95eecdb29e92e43de2
Layrovell <marina95prodan@gmail.com>
2021-01-24 14:32:25 +0200

    Updates

---
+++ precache-manifest.1c38ba2e298d5bbaf527dca6b94f20d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57462b0ba2e89169872ab9b6710347f3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3cc699ec8d78d602abd1",
+    "url": "./static/css/2.d6682f4f.chunk.css"
+  },
+  {
+    "revision": "002ce0d8639794fbe1ec",
+    "url": "./static/css/main.3abb6682.chunk.css"
+  },
+  {
+    "revision": "3cc699ec8d78d602abd1",
+    "url": "./static/js/2.9ac72d25.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.9ac72d25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "002ce0d8639794fbe1ec",
+    "url": "./static/js/main.e6d41507.chunk.js"
+  },
+  {
+    "revision": "53d1114eedfc0eaca354",
+    "url": "./static/js/runtime-main.a6df9e19.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.cb55ce10f2eb2a4d10df16c6247cd130.js"
+  "./precache-manifest.1c38ba2e298d5bbaf527dca6b94f20d6.js"
 );
 
 self.addEventListener('message', (event) => {
